ecbdbb114c35574bbce5b4733ab4099f7deca578
Tonia Del Priore <tonjagod@gmail.com>
2023-09-23 20:57:18 -0400

    add local conditiona to vit config, fix event handler

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/palindrome-site',
-  plugins: [react()],
+export default defineConfig(({ command }) => {
+  const config = {
+    base: '/',
+    plugins: [react()],
+  }
+
+  if (command !== 'serve') {
+    config.base = '/palindrome-site/';
+  }
+
+  return config;
 })
